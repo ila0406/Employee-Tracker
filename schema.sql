90f009f738ae541325e3ca9994439b79ec9351ac
Delilah <ila0406@gmail.com>
2022-03-12 10:45:53 -0700

    added DB folder and schema.sql file

---
+++ schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS sample_db;
+CREATE DATABASE company_db;
+
+
+-- TABLES
+-- departments
+    -- department.ID
+    -- department.name
+    -- department.created_time
+    -- department.updated_time
+    -- department.deleted_time
+-- roles
+    -- roles.ID
+    -- roles.job_title
+    -- roles.department_id
+    -- roles.salary
+    -- roles.created_time
+    -- roles.updated_time
+    -- roles.deleted_time
+-- employees
+    -- employees.ID
+    -- employees.first_name
+    -- employees.last_name
+    -- employees.manager_id
+    -- employees.role_id
+    -- employees.department_id
+    -- employees.created_time
+    -- employees.updated_time
+    -- employees.deleted_time
+-- company
