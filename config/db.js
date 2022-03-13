dd3732afee78e6bf68a5a19cf2028036fca55304
Delilah <ila0406@gmail.com>
2022-03-13 12:30:52 -0600

    added DB config

---
+++ config/db.js
@@ -0,0 +1,7 @@
+const config = {
+    // database_host: "m7az7525jg6ygibs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    // database_name: "yp9357h5nbbfteou",
+    // user_name: "lnh54nabkchziogl",
+    // user_password: "mhg33hno4l6cr7db",
+    combined_url: process.env.JAWSDB_URL,
+}
