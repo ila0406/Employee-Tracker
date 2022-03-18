1c8b2ec1d885f5c445b0fa84d6f56c16f0c30348
Delilah <ila0406@gmail.com>
2022-03-18 17:54:15 -0600

    tested schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,55 @@
 DROP DATABASE IF EXISTS employee_db;
-DROP TABLES IF EXISTS  department;
-DROP TABLES IF EXISTS  role;
 DROP TABLES IF EXISTS  employee;
+DROP TABLES IF EXISTS  role;
+DROP TABLES IF EXISTS  department;
 
 CREATE DATABASE employee_db;
 USE employee_db;
 
+CREATE TABLE department   (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    created_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_time DATETIME,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role   (
+    -- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT,
+    created_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_time DATETIME,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
 CREATE TABLE employee   (
+    -- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    manager_id
-    role_id
-    department_id
-    created_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_time TIMESTAMP,
-    deleted_time TIMESTAMP,
-    PRIMARY KEY (id)
+    manager_id INT,
+    role_id INT,
+    department_id INT,
+    created_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    deleted_time DATETIME,
+    PRIMARY KEY (id),
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-    FOREIGN KEY (managers_id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
     REFERENCES employee(id)
+    ON DELETE SET NULL
 );
 
 -- TABLES
