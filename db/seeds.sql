88443e61296771483168c44b435a70d5359da39a
Delilah <ila0406@gmail.com>
2022-03-18 18:10:29 -0600

    tested seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,39 @@
-INSERT INTO customers (id, first_name, last_name)
-VALUES (001, "Arthur", "Miller"),
-       (002, "Chinua", "Achebe"),
-       (003, "Margaret", "Atwood"),
-       (004, "Gabriel", "Garcia Marquez"),
-       (005, "Simone", "de Beauvoir");
-       
-INSERT INTO customer_order (id, customer_id, order_details)
-VALUES (001, 001, "coffee, milk"),
-       (002, 002, "eggs, cheese"),
-       (003, 003, "organic cereal"),
-       (004, 004, "oranges, peaches"),
-       (005, 005, "ice cream");
-       
+INSERT INTO department (name)
+VALUES 
+    ('IT'),
+    ('Product'),
+    ('Sales'),
+    ('Customer Sucess'),
+    ('Engineering'),
+    ('Operations'),
+    ('Marketing');
+
+SELECT * FROM department;
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+    ('Manager', 10000.00, 1),
+    ('Manager', 10000.00, 2),
+    ('Manager', 10000.00, 3),
+    ('Manager', 10000.00, 4),
+    ('Manager', 10000.00, 5),
+    ('Manager', 10000.00, 6),
+    ('Manager', 10000.00, 7);
+
+SELECT * FROM role;
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id, department_id)
+VALUES 
+    ('Delilah', 'Haas', NULL, 1, 1),
+    ('Crispin', 'Bannerman', 1, 1, 1),
+    ('Liz', 'Erd', 1, 1, 1),
+    ('Joe', 'Smith',  1, 1, 1),
+    ('Elle', 'Phant',  1, 1, 1),
+    ('Rose', 'Bush',  1, 1, 1),
+    ('Ray', 'OSun',  1, 1, 1),
+    ('Tera', 'Dactycl',  1, 1, 1),
+    ('Pin', 'Gwen',  1, 1, 1),
+    ('Janey', 'Doe',  1, 1, 1),
+    ('Alli', 'Gater',  1, 1, 1);
+
+ SELECT * FROM employee;   
