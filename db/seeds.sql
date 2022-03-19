5106e6a0084decbd75232705b880bbf900643f02
Delilah <ila0406@gmail.com>
2022-03-19 09:44:04 -0600

    added a bunch of random code to play with

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,28 +12,31 @@ SELECT * FROM department;
 
 INSERT INTO role (title, salary, department_id)
 VALUES 
-    ('Manager', 10000.00, 1),
-    ('Manager', 10000.00, 2),
-    ('Manager', 10000.00, 3),
-    ('Manager', 10000.00, 4),
-    ('Manager', 10000.00, 5),
-    ('Manager', 10000.00, 6),
-    ('Manager', 10000.00, 7);
+    ('Manager', 100000.00, 1),
+    ('Business Analyst', 99000.00, 6),
+    ('Product Manager', 100000.00, 3),
+    ('Implementation Specalist', 90000.00, 4),
+    ('Consultant', 90000.00, 4),
+    ('AccountManager', 100000.00, 4),
+    ('Engineer I', 70000.00, 5),
+    ('Engineer II', 80000.00, 5),
+    ('Engineer III', 90000.00, 5);
 
 SELECT * FROM role;
 
 INSERT INTO employee (first_name, last_name, manager_id, role_id, department_id)
 VALUES 
-    ('Delilah', 'Haas', NULL, 1, 1),
-    ('Crispin', 'Bannerman', 1, 1, 1),
-    ('Liz', 'Erd', 1, 1, 1),
-    ('Joe', 'Smith',  1, 1, 1),
-    ('Elle', 'Phant',  1, 1, 1),
-    ('Rose', 'Bush',  1, 1, 1),
-    ('Ray', 'OSun',  1, 1, 1),
-    ('Tera', 'Dactycl',  1, 1, 1),
-    ('Pin', 'Gwen',  1, 1, 1),
-    ('Janey', 'Doe',  1, 1, 1),
-    ('Alli', 'Gater',  1, 1, 1);
+    ('Delilah', 'Haas', NULL, 1, 6),
+    ('Crispin', 'Bannerman', 1, 5, 3),
+    ('Liz', 'Erd', 1, 2, 6),
+    ('Joe', 'Smith',  1, 2, 6),
+    ('Elle', 'Phant',  1, 4, 4),
+    ('Rose', 'Bush',  1, 5, 4),
+    ('Ray', 'OSun',  1, 6, 4),
+    ('Tera', 'Dactycl',  1, 9, 5),
+    ('Pin', 'Gwen',  8, 7, 5),
+    ('Janey', 'Doe',  8, 8, 5),
+    ('Alli', 'Gater',  8, 9, 5);
 
- SELECT * FROM employee;   
+--  SELECT * FROM employee;
+ SELECT e.id, e.first_name as FIRST, e.last_name as LAST, e.manager_id as Manager, r.title as Role_title, d.name as Department FROM employee e, role r, department d WHERE e.role_id = r.id and e.department_id = d.id order by d.id,r.id;
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const db = mysql.createConnection(
     user: 'root',
     // MySQL password
     password: '',
-    database: 'classlist_db'
+    database: 'employee_db'
   },
   console.log(`Connected to the classlist_db database.`)
 );
@@ -42,6 +42,98 @@ const db = mysql.createConnection(
 
 
 // 3) questions from inquirer
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'command',
+            message: 'What would you like to do?',
+            choices: ['View All Employees','Add Employee','Remove Employee','Update Employee Role','Update Employee Manager','Update Employee Manager','View All Roles','Add Role','View all Departments','Add Department','View All Employees By Department','View All Employees by Manager','Quit']
+        },
+        {
+            type: 'input',
+            message: "Which employee's role do you want to update?",
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'Which role do you want to assign the selected employee?',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: "Who is the name of the department",
+            name: 'nameDepartment'
+        },
+        {
+            type: 'input',
+            message: "Who is the name of the role",
+            name: 'nameRole'
+        },
+        {
+            type: 'input',
+            message: "Who is the first name of the employee",
+            name: 'nameFirst'
+        },
+        {
+            type: 'input',
+            message: "Who is the last name of the employee",
+            name: 'nameLast'
+        },
+        {
+            type: 'input',
+            message: 'Enter office number:',
+            name: 'officeNumber',
+            when: (input) => input.role === "Manager",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Enter engineer's github name:",
+            when: (input) => input.role === "Engineer",
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Enter the intern's school:",
+            when: (input) => input.role === "Intern",
+        },
+        {
+            type: 'confirm',
+            name: 'moreEmployees',
+            message: 'Would you like to add more employees to your team?',
+            default: false
+        }
+    ])
+    .then (employeeResponse => {
+        // takes users input to generate employee cards for HTML export
+        let { name, id, email, role, officeNumber, github, school, moreEmployees } = employeeResponse; 
+        let employee; 
+        let employeeObject;
+
+        if (role === "Manager") {
+            employeeObject= new Manager (name, id, email, officeNumber);
+            employee = generateManagerCard(employeeObject);
+        }
+        else if (role === "Engineer") {
+            employeeObject = new Engineer (name, id, email, github);
+            employee = generateEngrCard(employeeObject);
+
+        } else if (role === "Intern") {
+            employeeObject = new Intern (name, id, email, school);
+            employee = generateInternCard(employeeObject);
+        }
+
+        teamMembers.push(employee); 
+
+        // Keep cycling through questions prompt if more team members are needed
+        if (moreEmployees) {
+            return questions(); 
+        } else {
+            return teamMembers;
+        }
+    }) 
+};
 
 // 4) function to navigate options
 
@@ -49,3 +141,35 @@ const db = mysql.createConnection(
 // 6) Departments
 // 7) Managers
 // 8) Roles
+
+// variables needed for this application
+// const {generateHTML, generateManagerCard, generateInternCard, generateEngrCard} = require('./lib/generateHTML');
+// const Engineer = require("./lib/engineer.js");
+// const Intern = require("./lib/intern.js");
+// const Manager = require("./lib/manager.js");
+const teamMembers = [];
+
+// packages needed for this application
+// const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+// Function to write HTML file
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, generateHTML(data.join('')), function(err) { 
+//         console.log(err)
+//     })
+// }
+
+// Function to initialize app
+function init() {
+    questions()
+        .then(answers => {
+            console.log(answers)
+            //  writeToFile('./dist/index.html',answers);
+        }
+    );
+}
+
+// Function call to initialize app
+init();
