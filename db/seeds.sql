102826217dab1bf51b98cd46c80e0481385d6f74
Delilah <ila0406@gmail.com>
2022-03-24 09:43:07 -0600

    added readme

--- db/seeds.sql
+++ db/seeds.sql
@@ -20,23 +20,25 @@ VALUES
     ('AccountManager', 100000.00, 4),
     ('Engineer I', 70000.00, 5),
     ('Engineer II', 80000.00, 5),
-    ('Engineer III', 90000.00, 5);
+    ('Engineer III', 90000.00, 5),
+    ('CEO', 90000.00, 5);
 
 SELECT * FROM role;
 
 INSERT INTO employee (first_name, last_name, manager_id, role_id, department_id)
 VALUES 
-    ('Delilah', 'Haas', NULL, 1, 6),
-    ('Crispin', 'Bannerman', 1, 5, 3),
-    ('Liz', 'Erd', 1, 2, 6),
-    ('Joe', 'Smith',  1, 2, 6),
-    ('Elle', 'Phant',  1, 4, 4),
-    ('Rose', 'Bush',  1, 5, 4),
-    ('Ray', 'OSun',  1, 6, 4),
-    ('Tera', 'Dactycl',  1, 9, 5),
-    ('Pin', 'Gwen',  8, 7, 5),
-    ('Janey', 'Doe',  8, 8, 5),
-    ('Alli', 'Gater',  8, 9, 5);
+    ('CEO', 'Jane', NULL, 1, 6),
+    ('Delilah', 'Haas', 2, 1, 6),
+    ('Crispin', 'Bannerman', 2, 5, 3),
+    ('Liz', 'Erd', 2, 2, 6),
+    ('Joe', 'Smith',  2, 2, 6),
+    ('Elle', 'Phant',  2, 4, 4),
+    ('Rose', 'Bush',  2, 5, 4),
+    ('Ray', 'OSun',  2, 6, 4),
+    ('Tera', 'Dactycl',  2, 9, 5),
+    ('Pin', 'Gwen',  9, 7, 5),
+    ('Janey', 'Doe',  9, 8, 5),
+    ('Alli', 'Gater',  9, 9, 5);
 
 --  SELECT * FROM employee;
  SELECT e.id, e.first_name as FIRST, e.last_name as LAST, e.manager_id as Manager, r.title as Role_title, d.name as Department FROM employee e, role r, department d WHERE e.role_id = r.id and e.department_id = d.id order by d.id,r.id;
--- server.js
+++ server.js
@@ -49,37 +49,37 @@ async function init ()  {
             await allEmployees();
             break;
         case '02) Add Employee':
-            await allEmployees();
+            await addEmployee();
             break;
         case '03) Remove Employee':
-            await allEmployees();
+            await removeEmployee();
             break;
         case '04) Update Employee Role':
-            await allEmployees();
+            await updateEmployeeRole();
             break;
         case '05) Update Employee Manager':
-            await allEmployees();
+            await updateEmployeeManager();
             break;
         case '06) Update Employee Department':
-            await allEmployees();
+            await updateEmployeeDept();
             break;
         case '07) View All Roles':
             await allRoles();
             break;
         case '08) Add Role':
-            console.log('Option8');
+            await addRole();
             break;
         case '09) View all Departments':
             await allDepartments();
             break;
         case '10) Add Department':
-            await allEmployees();
+            await addDepartment();
             break;
         case '11) View All Employees By Department':
-            await allEmployees();
+            await allEmployeesByDept();
             break;
         case '12) View All Employees by Manager':
-            await allEmployees();
+            await allEmployeesByManager();
             break;
         case 'Quit':
             process.exit();
@@ -92,7 +92,8 @@ async function init ()  {
 
 // Menu option 01) View All Employees
 function allEmployees() {
-    const query = `SELECT id, first_name, last_name, manager_id, role_id, department_id FROM employee WHERE deleted_time is NULL;`;
+    const query = `SELECT CONCAT(e.first_name, " ", e.last_name) AS Name, role.title AS Title, role.salary AS Salary, CONCAT(m.first_name, " ", m.last_name) AS Manager, department.name AS Department FROM employee e INNER JOIN role ON role_id=role.id INNER JOIN employee m ON e.manager_id=m.id INNER JOIN department ON role.department_id=department.id;
+    `;
     db.query(query, (err, res) => {
         if (err) throw err;
         console.log('\n');
@@ -102,85 +103,165 @@ function allEmployees() {
     });
 };
 
-// // Menu option 02) Add Employee
-// function xxxxxx() {
+// Menu option 02) Add Employee
+async function addEmployee() {
+    // console.log('\n');
+    // console.log('Bugs found');
+    // init();
+    console.log('ADDING EMPLOYEE');
+
+    const addname = await inquirer.prompt(namePrompt());
+
+    db.query('SELECT * FROM role ORDER BY role.id;', async (err, res) => {
+        if (err) throw err;
+        const { role } = await inquirer.prompt([
+            {
+                name: 'role',
+                type: 'list',
+                choices: () => res.map(res => res.title),
+                message: 'What is the employees role?: '
+            }
+        ]);
+
+        let roleId;
+        for (const row of res) {
+            if (row.title === role) {
+                roleId = row.id;
+                continue;
+            }
+        }
+        db.query('SELECT * FROM employee', async (err, res) => {
+            if (err) throw err;
+            let choices = res.map(res => `${res.first_name} ${res.last_name}`);
+            choices.push('none');
+            let { manager } = await inquirer.prompt([
+                {
+                    name: 'manager',
+                    type: 'list',
+                    choices: choices,
+                    message: 'Choose the employee Manager: '
+                }
+            ]);
+            let managersID;
+            let managerName;
+            if (manager === 'none') {
+                managersID = null;
+            } else {
+                for (const data of res) {
+                    data.fullName = `${data.first_name} ${data.last_name}`;
+                    if (data.fullName === manager) {
+                        managersID = data.id;
+                        managerName = data.fullName;
+                        console.log(managersID);
+                        console.log(managerName);
+                        continue;
+                    }
+                }
+            }
+            
+            db.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: addname.first,
+                    last_name: addname.last,
+                    role_id: roleId,
+                    manager_id: parseInt(managersID)
+                },
+                (err, res) => {
+                    if (err) throw err;
+                    init();
+                }
+            );
+        });
+    });
+};
+
+function namePrompt() {
+    return ([
+        {
+            name: "first",
+            type: "input",
+            message: "Enter the first name: "
+        },
+        {
+            name: "last",
+            type: "input",
+            message: "Enter the last name: "
+        }
+    ]);
+}
+
+// Menu option 03) Remove Employee
+function removeEmployee() {
 //     const query = ``;
 //     db.query(query, (err, res) => {
 //         if (err) throw err;
-//         console.log('\n');
-//         console.log('');
+        console.log('\n');
+        console.log('Removing a Employee will be in v2');
 //         console.table(res);
 //         init();
 //     });
-// };
-// // Menu option 03) Remove Employee
-// function xxxxxx() {
+};
+
+// Menu option 04) Update Employee Role
+function updateEmployeeRole() {
 //     const query = ``;
 //     db.query(query, (err, res) => {
 //         if (err) throw err;
-//         console.log('\n');
-//         console.log('');
+        console.log('\n');
+        console.log('Updating a employees Role will be in v2');
 //         console.table(res);
 //         init();
 //     });
-// };
-// // Menu option 04) Update Employee Role
-// function xxxxxx() {
+};
+
+// Menu option 05) Update Employee Manager
+function updateEmployeeManager() {
 //     const query = ``;
 //     db.query(query, (err, res) => {
 //         if (err) throw err;
-//         console.log('\n');
-//         console.log('');
+        console.log('\n');
+        console.log('Updating a employees Manager will be in v2');
 //         console.table(res);
 //         init();
 //     });
-// };
-// // Menu option 05) Update Employee Manager
-// function xxxxxx() {
+};
+
+// Menu option 06) Update Employee Department
+function updateEmployeeDept() {
 //     const query = ``;
 //     db.query(query, (err, res) => {
 //         if (err) throw err;
-//         console.log('\n');
-//         console.log('');
+        console.log('\n');
+        console.log('Updating a employees Department will be in v2');
 //         console.table(res);
 //         init();
 //     });
-// };
-// // Menu option 06) Update Employee Department
-// function xxxxxx() {
-//     const query = ``;
-//     db.query(query, (err, res) => {
-//         if (err) throw err;
-//         console.log('\n');
-//         console.log('');
-//         console.table(res);
-//         init();
-//     });
-// };
+};
 
 // Menu option 07) View All Roles
 function allRoles() {
-    const query = `SELECT r.id, r.title, r.salary, d.name FROM role r, department d WHERE d.id = r.department_id and d.deleted_time is NULL;`;
+    const query = `SELECT r.id, r.title as role_title, r.salary, d.name as department_name FROM role r, department d WHERE d.id = r.department_id and d.deleted_time is NULL;`;
     db.query(query, (err, res) => {
         if (err) throw err;
         console.log('\n');
-        console.log('VIEW ALL DEPARTMENTS');
+        console.log('VIEW ALL ROLES');
         console.table(res);
         init();
     });
 };
 
-// // Menu option 08) Add Role
-// function xxxxxx() {
+// Menu option 08) Add Role
+function addRole() {
 //     const query = ``;
 //     db.query(query, (err, res) => {
 //         if (err) throw err;
-//         console.log('\n');
-//         console.log('');
+        console.log('\n');
+        console.log('Adding a Role will be in v2');
 //         console.table(res);
 //         init();
 //     });
-// };
+};
 
 // Menu option 09) View all Departments
 function allDepartments() {
@@ -194,41 +275,41 @@ function allDepartments() {
     });
 };
 
-// // Menu option 10) Add Department
-// function xxxxxx() {
+// Menu option 10) Add Department
+function addDepartment() {
 //     const query = ``;
 //     db.query(query, (err, res) => {
 //         if (err) throw err;
-//         console.log('\n');
-//         console.log('');
+        console.log('\n');
+        console.log('Adding a Department will be in v2');
 //         console.table(res);
 //         init();
 //     });
-// };
+};
 
-// // Menu option 11) View All Employees By Department
-// function xxxxxx() {
-//     const query = ``;
-//     db.query(query, (err, res) => {
-//         if (err) throw err;
-//         console.log('\n');
-//         console.log('');
-//         console.table(res);
-//         init();
-//     });
-// };
+// Menu option 11) View All Employees By Department
+function allEmployeesByDept() {
+    const query = `SELECT id, first_name, last_name, manager_id, role_id, department_id FROM employee WHERE deleted_time is NULL ORDER BY department_id;`;
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log('');
+        console.table(res);
+        init();
+    });
+};
 
-// // Menu option 12) View All Employees by Manager
-// function xxxxxx() {
-//     const query = ``;
-//     db.query(query, (err, res) => {
-//         if (err) throw err;
-//         console.log('\n');
-//         console.log('');
-//         console.table(res);
-//         init();
-//     });
-// };
+// Menu option 12) View All Employees by Manager
+function allEmployeesByManager() {
+    const query = `SELECT id, first_name, last_name, manager_id, role_id, department_id FROM employee WHERE deleted_time is NULL ORDER BY manager_id;`;
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log('');
+        console.table(res);
+        init();
+    });
+};
 
 
 // Function call to initialize app
